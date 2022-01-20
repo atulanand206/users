7634262e91463198488439a92039dda37f6c68a1
Atul Anand <atulanand206@gmail.com>
2022-01-21 00:05:22 +0530

    some structure to api errors.
    ratings converted to a map for type specific scores.

--- objects/user.go
+++ objects/user.go
@@ -3,26 +3,26 @@ package objects
 type (
 	// Definition of the UserRequest object.
 	UserRequest struct {
-		Username          string   `json:"username" bson:"username"`
-		Password          string   `json:"password" bson:"-"`
-		EncryptedPassword [32]byte `bson:"password"`
-		Name              string   `json:"name" bson:"name, omitempty"`
-		Rating            int      `json:"rating" bson:"rating, omitempty"`
+		Username          string         `json:"username" bson:"username"`
+		Password          string         `json:"password" bson:"-"`
+		EncryptedPassword [32]byte       `bson:"password"`
+		Name              string         `json:"name" bson:"name, omitempty"`
+		Rating            map[string]int `json:"rating" bson:"rating, omitempty"`
 	}
 
 	// Definition of the User object.
 	User struct {
-		Id       string `json:"id" bson:"_id, omitempty"`
-		Username string `json:"username" bson:"username"`
-		Name     string `json:"name" bson:"name, omitempty"`
-		Rating   int    `json:"rating" bson:"rating, omitempty"`
+		Id       string         `json:"id" bson:"_id, omitempty"`
+		Username string         `json:"username" bson:"username"`
+		Name     string         `json:"name" bson:"name, omitempty"`
+		Rating   map[string]int `json:"rating" bson:"rating, omitempty"`
 	}
 
 	// Definition of the UpdateUser object.
 	UpdateUser struct {
-		Username string `json:"username" bson:"username"`
-		Name     string `json:"name" bson:"name, omitempty"`
-		Rating   int    `json:"rating" bson:"rating, omitempty"`
+		Username string         `json:"username" bson:"username"`
+		Name     string         `json:"name" bson:"name, omitempty"`
+		Rating   map[string]int `json:"rating" bson:"rating, omitempty"`
 	}
 
 	// Definition of the AuthorizeRequest object.
---
+++ routes/error.go
@@ -0,0 +1,13 @@
+package routes
+
+const (
+	Err_Decode_Request          = "Failed to decode the request body"
+	Err_Decode_Response         = "Failed to decode the db response"
+	Err_Create_Document_Failed  = "Failed to create document for database"
+	Err_Create_Failed           = "DB entry create request failed"
+	Err_Update_Failed           = "DB entry update request failed"
+	Err_Read_Failed             = "DB entry read request failed"
+	Err_Invalid_Login           = "Invalid credentials"
+	Err_Unauthorized            = "Unauthorized access"
+	Err_Token_Generation_Failed = "Token generation failed"
+)
